dd126b792001a69ef3d1f6bc9acb9259b52a1a4a
Venkatesh <venkatesh123@gmail.com>
2025-03-10 21:03:40 +0530

    change code projects.py

--- projects.py
+++ projects.py
@@ -1,1 +1,19 @@
-sathaiah
+import random
+game_rule = ["stone" , "paper" , "scisser"]
+user_choice = int(input("enter your choice, you choose 1.stone, 2.paper and 3.scisser \n"))
+print(game_rule[user_choice])
+computer_choice = random.randint(0,2)
+print(game_rule[computer_choice])
+if user_choice == computer_choice:
+    print("match draw!")
+elif user_choice == 0 and computer_choice == 2:
+    print("you win!")
+elif user_choice == 2 and computer_choice == 0:
+    print("you lose!")
+elif  computer_choice > user_choice :
+    print("you lose!")
+elif computer_choice < user_choice:
+    print("you win")
+else:
+    print("enter a valid input")
+    
